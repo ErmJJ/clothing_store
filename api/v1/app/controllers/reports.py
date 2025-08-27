4a3194124060ae58b60e7baea43f302baa0d8dc9
ErmJJ <jjuli714cr@outlook.com>
2025-08-27 05:41:15 -0600

    Finalizacion del proyecto de Desarrollo con Plataformas Abiertas

--- api/v1/app/controllers/reports.py
+++ api/v1/app/controllers/reports.py
@@ -1,27 +1,34 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from ..models.reports import reportsModel
 
 reports_endpoint = Blueprint('reports_endpoint', __name__)
 
+# 1. Listado de todas las marcas que tienen al menos una venta
 @reports_endpoint.route('/reports/brands-with-sales', methods=['GET'])
 def get_brands_with_sales():
     data = reportsModel.brands_with_sales()
     return jsonify(data), 200
 
-@reports_endpoint.route('/reports/sold-products-stock', methods=['GET'])
-def get_sold_products_stock():
-    data = reportsModel.sold_products_stock()
+# 2. Prendas vendidas y su cantidad restante en stock
+@reports_endpoint.route('/reports/products-stock', methods=['GET'])
+def get_products_stock():
+    data = reportsModel.products_sold_and_stock()
     return jsonify(data), 200
 
-@reports_endpoint.route('/reports/top-5-brands', methods=['GET'])
-def get_top_5_brands():
+# 3. Top 5 marcas más vendidas y su cantidad de ventas
+@reports_endpoint.route('/reports/top-brands', methods=['GET'])
+def get_top_brands():
     data = reportsModel.top_5_brands()
     return jsonify(data), 200
 
-@reports_endpoint.route('/reports/sales-by-date', methods=['GET'])
-def get_sales_by_date():
-    date = request.args.get('date')
-    if not date:
-        return jsonify({"error": "Parámetro 'date' requerido"}), 400
-    data = reportsModel.sales_by_date(date)
+# 4. Usuarios con más compras realizadas
+@reports_endpoint.route('/reports/top-users', methods=['GET'])
+def get_top_users():
+    data = reportsModel.top_users()
+    return jsonify(data), 200
+
+# 5. Promedio de calificación por producto
+@reports_endpoint.route('/reports/product-ratings', methods=['GET'])
+def get_product_ratings():
+    data = reportsModel.average_ratings()
     return jsonify(data), 200
--- api/v1/app/models/reports.py
+++ api/v1/app/models/reports.py
@@ -1,92 +1,143 @@
+from bson.objectid import ObjectId
 from app.index import mongo
 
 class reportsModel:
+
+    # 1. Listado de todas las marcas que tienen al menos una venta
     @staticmethod
     def brands_with_sales():
-        print(mongo.db.list_collection_names())
         pipeline = [
-            {"$lookup": {
-                "from": "products",
-                "localField": "product_id",
-                "foreignField": "_id",
-                "as": "producto"
-            }},
-            {"$unwind": "$producto"},
-            {"$group": {
-                "_id": "$producto.brand_id"
-            }},
-            {"$lookup": {
-                "from": "brands",
-                "localField": "_id",
-                "foreignField": "_id",
-                "as": "marca"
-            }},
-            {"$unwind": "$marca"},
-            {"$project": {
-                "brand_id": {"$toString": "$_id"},
-                "name": "$marca.name",
-                "country": "$marca.country"
-            }}
+            {
+                "$lookup": {
+                    "from": "products",
+                    "localField": "_id",
+                    "foreignField": "brand_id",
+                    "as": "products"
+                }
+            },
+            { "$unwind": "$products" },
+            {
+                "$lookup": {
+                    "from": "sales",
+                    "localField": "products._id",
+                    "foreignField": "product_id",
+                    "as": "sales"
+                }
+            },
+            { "$match": { "sales": { "$ne": [] } } },
+            {
+                "$project": {
+                    "_id": { "$toString": "$_id" },
+                    "name": 1,
+                    "country": 1
+                }
+            },
+            { "$group": { "_id": "$_id", "name": { "$first": "$name" }, "country": { "$first": "$country" } } }
         ]
-        return list(mongo.db.sales.aggregate(pipeline))
+        return list(mongo.db.brands.aggregate(pipeline))
 
+    # 2. Prendas vendidas y su cantidad restante en stock
     @staticmethod
-    def sold_products_stock():
-        print(mongo.db.list_collection_names())
+    def products_sold_and_stock():
         pipeline = [
-            {"$group": {
-                "_id": "$product_id",
-                "vendido": {"$sum": "$quantity"}
-            }},
-            {"$lookup": {
-                "from": "products",
-                "localField": "_id",
-                "foreignField": "_id",
-                "as": "producto"
-            }},
-            {"$unwind": "$producto"},
-            {"$project": {
-                "product_id": {"$toString": "$_id"},
-                "name": "$producto.name",
-                "vendido": 1,
-                "stock_restante": "$producto.stock"
-            }}
+            {
+                "$lookup": {
+                    "from": "sales",
+                    "localField": "_id",
+                    "foreignField": "product_id",
+                    "as": "sales"
+                }
+            },
+            {
+                "$project": {
+                    "_id": { "$toString": "$_id" },
+                    "name": 1,
+                    "stock": 1,
+                    "sold_quantity": { "$sum": "$sales.quantity" }
+                }
+            }
         ]
-        return list(mongo.db.sales.aggregate(pipeline))
+        return list(mongo.db.products.aggregate(pipeline))
 
+    # 3. Top 5 marcas más vendidas
     @staticmethod
     def top_5_brands():
-        print(mongo.db.list_collection_names())
         pipeline = [
-            {"$lookup": {
-                "from": "products",
-                "localField": "product_id",
-                "foreignField": "_id",
-                "as": "producto"
-            }},
-            {"$unwind": "$producto"},
-            {"$group": {
-                "_id": "$producto.brand_id",
-                "ventas_totales": {"$sum": "$quantity"}
-            }},
-            {"$sort": {"ventas_totales": -1}},
-            {"$limit": 5},
-            {"$lookup": {
-                "from": "brands",
-                "localField": "_id",
-                "foreignField": "_id",
-                "as": "marca"
-            }},
-            {"$unwind": "$marca"},
-            {"$project": {
-                "brand_id": {"$toString": "$_id"},
-                "name": "$marca.name",
-                "ventas_totales": 1
-            }}
+            {
+                "$lookup": {
+                    "from": "products",
+                    "localField": "_id",
+                    "foreignField": "brand_id",
+                    "as": "products"
+                }
+            },
+            { "$unwind": "$products" },
+            {
+                "$lookup": {
+                    "from": "sales",
+                    "localField": "products._id",
+                    "foreignField": "product_id",
+                    "as": "sales"
+                }
+            },
+            { "$unwind": "$sales" },
+            {
+                "$group": {
+                    "_id": { "$toString": "$_id" },
+                    "brand_name": { "$first": "$name" },
+                    "total_sales": { "$sum": "$sales.quantity" }
+                }
+            },
+            { "$sort": { "total_sales": -1 } },
+            { "$limit": 5 }
         ]
-        return list(mongo.db.sales.aggregate(pipeline))
+        return list(mongo.db.brands.aggregate(pipeline))
 
+    # 4. Usuarios con más compras realizadas
     @staticmethod
-    def sales_by_date(date):
-        print(mongo.db.list_collection_names())
-        return list(mongo.db.sales.find({"sale_date": date}))
+    def top_users():
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": "sales",
+                    "localField": "_id",
+                    "foreignField": "user_id",
+                    "as": "sales"
+                }
+            },
+            {
+                "$project": {
+                    "_id": { "$toString": "$_id" },
+                    "username": 1,
+                    "email": 1,
+                    "total_purchases": { "$size": "$sales" }
+                }
+            },
+            { "$sort": { "total_purchases": -1 } },
+            { "$limit": 5 }
+        ]
+        return list(mongo.db.users.aggregate(pipeline))
+
+    # 5. Promedio de calificación por producto
+    @staticmethod
+    def average_ratings():
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": "reviews",
+                    "localField": "_id",
+                    "foreignField": "product_id",
+                    "as": "reviews"
+                }
+            },
+            {
+                "$project": {
+                    "_id": { "$toString": "$_id" },
+                    "name": 1,
+                    "avg_rating": { "$avg": "$reviews.rating" },
+                    "total_reviews": { "$size": "$reviews" }
+                }
+            },
+            { "$sort": { "avg_rating": -1 } }
+        ]
+        return list(mongo.db.products.aggregate(pipeline))
